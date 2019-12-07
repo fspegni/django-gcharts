4152fd176eb027b07ed4fe2f6a5d40530136a368
fspegni <francesco.spegni@gmail.com>
2019-12-07 17:38:06 +0100

    fix incompatibility due to python3

--- gcharts/contrib/gviz_api.py
+++ gcharts/contrib/gviz_api.py
@@ -27,7 +27,8 @@ Google Visualization API.
 __author__ = "Amit Weinstein, Misha Seltzer, Jacob Baskin"
 
 import cgi
-import cStringIO
+#import cStringIO
+import StringIO
 import csv
 import datetime
 try:
@@ -862,7 +863,9 @@ class DataTable(object):
       DataTableException: The data does not match the type.
     """
 
-    csv_buffer = cStringIO.StringIO()
+#    csv_buffer = cStringIO.StringIO()
+    csv_buffer = StringIO.StringIO()
+
     writer = csv.writer(csv_buffer, delimiter=separator)
 
     if columns_order is None:
